6abc7f9bcc274e3a96733a58e4c0972feb140a23
Wang_tao <Wang_tao@gmail.com>
2019-01-20 17:51:59 +0800

    绘制背景和放置飞船

---
+++ settings.py
@@ -0,0 +1,12 @@
+#coding=GBK
+
+class Settings():
+    """存储外星人入侵游戏的所有设置类"""
+    
+    def __init__(self):
+        """初始化游戏的设置"""
+        """屏幕设置"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
